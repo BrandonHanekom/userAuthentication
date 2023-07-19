483381ab2a89d6bbacdc6a6009be396f70bad6c1
BrandonHanekom <brandonhanekom00@gmail.com>
2023-07-19 16:28:40 +0200

    fixed table bug

--- index.php
+++ index.php
@@ -1,4 +1,53 @@
-<?php include 'connection.php'; ?>
+<?php // Connect to the database
+include 'connection.php';
+
+// Perform the query
+$sql = "SELECT books.book_name, authors.author_name, books.year, books.genre, books.age_group
+        FROM books
+        JOIN authors ON books.author_id = authors.author_id";
+$result = mysqli_query($conn, $sql);
+
+if (!$result) {
+    // Display the error message if the query fails
+    echo "Error: " . mysqli_error($conn);
+} else {
+    // Debugging: Print the generated SQL query
+    echo "SQL Query: " . $sql . "<br>";
+
+    // Debugging: Display the result set
+    var_dump($result);
+
+    // Display the table
+    echo "<table>";
+    echo "<thead>";
+    echo "<tr>";
+    echo "<th>Book Name</th>";
+    echo "<th>Author Name</th>";
+    echo "<th>Year</th>";
+    echo "<th>Genre</th>";
+    echo "<th>Age Group</th>";
+    echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+
+    // Loop through the result set and display the table rows
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td>" . $row["book_name"] . "</td>";
+        echo "<td>" . $row["author_name"] . "</td>";
+        echo "<td>" . $row["year"] . "</td>";
+        echo "<td>" . $row["genre"] . "</td>";
+        echo "<td>" . $row["age_group"] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</tbody>";
+    echo "</table>";
+}
+
+// // Close the database connection
+// mysqli_close($conn);
+?>
 
 <!DOCTYPE html>
 <html>
@@ -10,7 +59,7 @@
 
 <body>
     <div class="container">
-        <h2 class="mt-5">Library Index</h2>
+        <h2 class="mt-5">Library</h2>
         <table class="table">
             <thead>
                 <tr>
@@ -27,7 +76,7 @@
                 // Fetch books and authors data from the tables
                 $sql = "SELECT books.book_name, authors.author_name, books.year, books.genre, books.age_group
                         FROM books
-                        INNER JOIN authors ON books.book_id = authors.book_id";
+                        INNER JOIN authors ON books.author_id = authors.author_id";
                 $result = mysqli_query($conn, $sql);
 
                 // Display data in the table rows
